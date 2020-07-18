f2116489388083da1374cddd6a6e47ec4a48d3a3
Ryan Lee <ryanm.lee11@gmail.com>
2020-07-18 21:43:45 +0000

    Challenge - Constructor, operator "new" with Objects, Calculator, and Accumulators

--- index.js
+++ index.js
@@ -172,3 +172,81 @@ let ladder = {
 
 
 ladder.up().up().down().showStep(); // 1
+
+
+//Constructor, operator "new"
+
+//Challenge 1 - Two functions – one object
+
+/*create functions A and B such as new A()==new B()?
+*/
+
+// function A() { ... }
+// function B() { ... }
+
+// let a = new A;
+// let b = new B;
+
+// alert( a == b ); // true
+
+//Solution 
+
+let obj = {};
+
+function A() { return obj; }
+function B() { return obj; }
+
+alert( new A() == new B() ); // true
+
+//Challenge 2 - Create calculator
+
+// Create a constructor function Calculator that creates objects with 3 methods:
+
+// read() asks for two values using prompt and remembers them in object properties.
+// sum() returns the sum of these properties.
+// mul() returns the multiplication product of these properties.
+
+
+//Solution
+
+function Calculator() {
+  this.sum = function() {
+    return this.x + this.y
+  },
+  this.mul = function() {
+    return this.x * this.y
+  },
+  this.read = function() {
+    this.x = +prompt('x?', 0);
+    this.y = +prompt('y?', 0);
+    };
+}
+
+let calculator2 = new Calculator();
+calculator2.read();
+
+alert( "Sum = " + calculator2.sum() );
+alert( "Multiply = " + calculator2.mul() );
+
+//Challenge 3 - Create new Accumulator
+/* Create a constructor function Accumulator(startingValue).
+
+Object that it creates should:
+
+Store the “current value” in the property value. The starting value is set to the argument of the constructor startingValue.
+The read() method should use prompt to read a new number and add it to value.
+In other words, the value property is the sum of all user-entered values with the initial value startingValue.
+*/
+
+function Accumulator(startingValue) {
+  this.value = startingValue;
+
+  this.read = function() {
+    this.value += +prompt('x?', 0);
+    };
+}
+
+let accumulator = new Accumulator(1); // initial value 1
+accumulator.read(); // adds the user-entered value
+accumulator.read(); // adds the user-entered value
+alert(accumulator.value); // shows the sum of these values
